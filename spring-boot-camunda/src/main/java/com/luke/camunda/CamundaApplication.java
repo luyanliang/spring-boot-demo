e3418d1b18026e4eb052eb3d4d4d4683bfde493a
luyanliang <lu_yanliang@leapmotor.com>
2020-09-19 16:12:16 +0800

    [feat] 添加BPM

---
+++ spring-boot-camunda/src/main/java/com/luke/camunda/CamundaApplication.java
@@ -0,0 +1,16 @@
+package com.luke.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LuYanLiang [lu_yanliang@leapmotor.com]
+ * @since 2020/9/18 17:20
+ */
+@SpringBootApplication
+public class CamundaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaApplication.class, args);
+    }
+}
