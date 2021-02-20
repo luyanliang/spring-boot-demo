5b13f30c07bd960fe097cd42349c5b8b88be211d
luyanliang <lu_yanliang@leapmotor.com>
2021-02-20 14:38:28 +0800

    feat: 工作流整体流程学习

--- spring-boot-rabbitmq/src/main/java/com/luke/rabbitmq/helloworld/Consumer.java
+++ spring-boot-rabbitmq/src/main/java/com/luke/rabbitmq/helloworld/Consumer.java
@@ -41,31 +41,16 @@ public class Consumer {
          * 第二个参数：是否自动确认收到消息，false代表手动编程来确认消息，这是MQ的推荐做法
          * 第三个参数：要传入DefaultConsumer的实现类
          */
-        channel.basicConsume(RabbitConstant.QUEUE_HELLOWORLD, false, new Receiver(channel));
-    }
-}
+        channel.basicConsume(RabbitConstant.QUEUE_HELLOWORLD, false, new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body);
+                System.out.println("消费者接收到的消息：" + message);
+                System.out.println("消息的TagID: " + envelope.getDeliveryTag());
 
-class Receiver extends DefaultConsumer {
-
-    private Channel channel;
-
-    /**
-     * 重写构造函数，Channel通道对象需要从外层传入，在handleDelivery中要用到
-     *
-     * @param channel
-     */
-    public Receiver(Channel channel) {
-        super(channel);
-        this.channel = channel;
-    }
-
-    @Override
-    public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-        String message = new String(body);
-        System.out.println("消费者接收到的消息：" + message);
-        System.out.println("消息的TagID: " + envelope.getDeliveryTag());
-
-        // false只确认签收当前的消息，设置为true的时候则代表签收该消费者所有未签收的消息
-        channel.basicAck(envelope.getDeliveryTag(), false);
+                // false只确认签收当前的消息，设置为true的时候则代表签收该消费者所有未签收的消息
+                channel.basicAck(envelope.getDeliveryTag(), false);
+            }
+        });
     }
 }
